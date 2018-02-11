498b73a9f843c8151eae9a087303b9c61e69530b
VirtualTam <virtualtam@flibidi.net>
2018-02-11 22:21:17 +0100

    frontend: minify built CSS
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 
 const STATIC_DIR = 'dmarc_reporting/static/dmarc_reporting'
@@ -10,6 +12,8 @@ gulp.task('sass', function() {
       'node_modules/bootstrap/scss/bootstrap.scss',
       'frontend/scss/*.scss',
     ]).pipe(sass())
+      .pipe(csso())
+      .pipe(rename({ suffix: '.min' }))
       .pipe(gulp.dest(STATIC_DIR + "/css"));
 });
 
