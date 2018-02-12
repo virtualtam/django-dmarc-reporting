25a39312c2a0264e1b9a5d75b2025ba9fc798088
VirtualTam <virtualtam@flibidi.net>
2018-02-12 01:04:42 +0100

    WIP: views: add feedback report list
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- dmarc_reporting/urls.py
+++ dmarc_reporting/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = 'dmarc_reporting'
 urlpatterns = [
     path('', views.domains_list, name='domains_list'),
+    path('domains/', views.domains_list, name='domains_list'),
+    path('feedbackreports/<int:domain_pk>', views.feedback_reports, name="feedback_reports"),
     path('login/', auth_views.LoginView.as_view(template_name='dmarc_reporting/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), {'next_page': 'dmarc_reporting:login'}, name='logout'),
 ]
--- dmarc_reporting/views.py
+++ dmarc_reporting/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django.shortcuts import render
 
-from .models import Domain
+from .models import Domain, FeedbackReport
 
 
 @login_required
@@ -16,5 +16,21 @@ def domains_list(request):
             'domains': Domain.objects.annotate(
                 num_reports=Count('publishedfeedbackpolicy')
             ).filter(num_reports__gt=0),
+        }
+    )
+
+
+@login_required
+def feedback_reports(request, domain_pk):
+    """List feedback reports for a given domain"""
+    domain = Domain.objects.get(pk=domain_pk)
+    reports = FeedbackReport.objects.filter(policy_published__domain=domain).order_by('-date_end')
+
+    return render(
+        request,
+        'dmarc_reporting/feedback_reports.html',
+        {
+            'domain': domain,
+            'reports': reports,
         }
     )
