df6501425e0da3a5ebe48120f2e04619ddf45beb
VirtualTam <virtualtam@flibidi.net>
2018-04-15 16:11:16 +0200

    docker: tail Gunicorn and Nginx logs
    See:
    - https://serverfault.com/questions/599103/make-a-docker-application-write-to-stdout
    - https://serverfault.com/questions/661454/tail-f-not-following-log-file-in-docker-container
    
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 su app -c 'python3 /app/manage.py collectstatic'
 su app -c 'python3 /app/manage.py migrate'
+
+busybox tail -F \
+    /var/log/gunicorn/access.log \
+    /var/log/gunicorn/error.log \
+    /var/log/nginx/access.log \
+    /var/log/nginx/error.log \
+    &
+
 /bin/s6-svscan /etc/services.d
